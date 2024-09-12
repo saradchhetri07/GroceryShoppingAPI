517367b987a072371704861dc44d2f3344583850
saradchhetri07 <saradchhetri20690@gmail.com>
2024-09-12 17:18:14 +0545

    created migrations table and super user seed file

---
+++ src/database/migrations/20240912103336_create_uses_table.ts
@@ -0,0 +1,52 @@
+import { Knex } from "knex";
+
+const TABLE_NAME = "users";
+
+/**
+ * Create table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.bigIncrements("id"); // Primary key
+
+    table.string("name").notNullable(); // User name
+    table.string("email").notNullable().unique(); // User email (must be unique)
+    table.string("password").notNullable(); // Password (hashed)
+
+    // User type - normal user (0) or superuser (1)
+    table.string("user_type").notNullable().defaultTo("normal");
+
+    // Timestamps
+    table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+    table
+      .bigInteger("created_by")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable(TABLE_NAME)
+      .onDelete("CASCADE");
+
+    table.timestamp("updated_at").nullable();
+
+    table
+      .bigInteger("updated_by")
+      .unsigned()
+      .references("id")
+      .inTable(TABLE_NAME)
+      .nullable()
+      .onDelete("SET NULL");
+  });
+}
+
+/**
+ * Drop table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
